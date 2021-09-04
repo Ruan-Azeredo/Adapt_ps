b47d9209c9a0091d6a8baf7003df745d1c75487c
Ruan-Azeredo <ruanazeredo@gmail.com>
2021-09-03 22:02:38 -0300

    segundo commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\MovieController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+
+Route::get('/movie', [MovieController::class, 'index'])->name('movie.index');
